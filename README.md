706d908ce0f3f5ea3183b2349ed5af47fd34aa77
Max <max.partenfelder@wilde-it.com>
2023-06-25 01:05:12 +0200

    feat: initial version

---
+++ README.md
@@ -0,0 +1,59 @@
+# go-config
+
+Simple application configuration library for Go
+
+## Installation
+
+```bash
+go get github.com/mxcd/go-config
+```
+
+## Usage
+
+```go
+package main
+
+import (
+	"github.com/mxcd/go-config/config"
+)
+
+func main() {
+	err := config.LoadConfig([]config.Value{
+		config.String("LOG_LEVEL").NotEmpty().Default("info"),
+		config.Bool("DEV_MODE").Default(false),
+
+		config.String("DB_HOST").NotEmpty().Default("localhost"),
+		config.Int("DB_PORT").Default(5432),
+		config.String("DB_USER").NotEmpty(),
+		config.String("DB_PASSWORD").NotEmpty().Sensitive(),
+
+		config.String("S3_HOST").NotEmpty().Default("localhost"),
+		config.Int("S3_PORT").Default(9000),
+		config.String("S3_ACCESS_KEY").NotEmpty(),
+		config.String("S3_SECRET_KEY").NotEmpty().Sensitive(),
+		config.String("S3_BUCKET").NotEmpty(),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	config.Print()
+}
+```
+
+The configuration struct will ensure, that required configuration values are present and that bool and int values are valid.  
+Sensitive values will be masked when printed.
+
+Values can be accessed via the `config.Get()` function.
+
+```go
+import (
+	"github.com/mxcd/go-config/config"
+)
+
+func someFunction() {
+  // ...
+  devMode := config.Get().Bool("DEV_MODE")
+  // ...
+}
+```
---
+++ config/config.go
@@ -0,0 +1,316 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"log"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/joho/godotenv"
+)
+
+type Value interface {
+	Descriptor() *Descriptor
+}
+
+type ValueType string
+
+const (
+	StringType ValueType = "string"
+	BoolType   ValueType = "bool"
+	IntType    ValueType = "int"
+)
+
+type TypeInfo struct {
+	Type ValueType
+}
+
+type Descriptor struct {
+	EnvionmentVariable string
+	Default            any
+	Provided           bool
+	Value              any
+	NotEmpty           bool
+	Sensitive          bool
+	TypeInfo           TypeInfo
+}
+
+type applicationConfigType struct {
+	StringValues map[string]*Descriptor
+	BoolValues   map[string]*Descriptor
+	IntValues    map[string]*Descriptor
+}
+
+var applicationConfig applicationConfigType
+
+func Get() *applicationConfigType {
+	return &applicationConfig
+}
+
+func (c *applicationConfigType) String(key string) string {
+	descriptor, ok := c.StringValues[key]
+	if !ok {
+		log.Panicf("Config value %s not found", key)
+	}
+	return descriptor.Value.(string)
+}
+
+func (c *applicationConfigType) Bool(key string) bool {
+	descriptor, ok := c.BoolValues[key]
+	if !ok {
+		log.Panicf("Config value %s not found", key)
+	}
+	return descriptor.Value.(bool)
+}
+
+func (c *applicationConfigType) Int(key string) int {
+	descriptor, ok := c.IntValues[key]
+	if !ok {
+		log.Panicf("Config value %s not found", key)
+	}
+	return descriptor.Value.(int)
+}
+
+type stringBuilder struct {
+	desc *Descriptor
+}
+
+func (b *stringBuilder) Descriptor() *Descriptor {
+	return b.desc
+}
+func String(environmentVariable string) *stringBuilder {
+	return &stringBuilder{
+		desc: &Descriptor{
+			EnvionmentVariable: environmentVariable,
+			NotEmpty:           false,
+			Sensitive:          false,
+			TypeInfo:           TypeInfo{Type: StringType},
+		},
+	}
+}
+func (b *stringBuilder) Default(defaultValue string) *stringBuilder {
+	b.desc.Default = defaultValue
+	return b
+}
+func (b *stringBuilder) NotEmpty() *stringBuilder {
+	b.desc.NotEmpty = true
+	return b
+}
+func (b *stringBuilder) Sensitive() *stringBuilder {
+	b.desc.Sensitive = true
+	return b
+}
+
+type boolBuilder struct {
+	desc *Descriptor
+}
+
+func (b *boolBuilder) Descriptor() *Descriptor {
+	return b.desc
+}
+func Bool(environmentVariable string) *boolBuilder {
+	return &boolBuilder{
+		desc: &Descriptor{
+			EnvionmentVariable: environmentVariable,
+			NotEmpty:           false,
+			Sensitive:          false,
+			TypeInfo:           TypeInfo{Type: BoolType},
+		},
+	}
+}
+func (b *boolBuilder) Default(defaultValue bool) *boolBuilder {
+	b.desc.Default = defaultValue
+	return b
+}
+func (b *boolBuilder) Sensitive() *boolBuilder {
+	b.desc.Sensitive = true
+	return b
+}
+
+type intBuilder struct {
+	desc *Descriptor
+}
+
+func (b *intBuilder) Descriptor() *Descriptor {
+	return b.desc
+}
+func Int(environmentVariable string) *intBuilder {
+	return &intBuilder{
+		desc: &Descriptor{
+			EnvionmentVariable: environmentVariable,
+			NotEmpty:           false,
+			Sensitive:          false,
+			TypeInfo:           TypeInfo{Type: IntType},
+		},
+	}
+}
+func (b *intBuilder) Default(defaultValue int) *intBuilder {
+	b.desc.Default = defaultValue
+	return b
+}
+func (b *intBuilder) Sensitive() *intBuilder {
+	b.desc.Sensitive = true
+	return b
+}
+
+func LoadConfig(values []Value) error {
+	if err := loadDotEnvFile(); err != nil {
+		log.Printf("No .env file found. Relying on environment variables.")
+	} else {
+		log.Printf("Loaded .env file.")
+	}
+
+	applicationConfig = applicationConfigType{
+		StringValues: make(map[string]*Descriptor),
+		BoolValues:   make(map[string]*Descriptor),
+		IntValues:    make(map[string]*Descriptor),
+	}
+
+	for _, value := range values {
+		if err := loadConfigValue(value.Descriptor()); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func loadDotEnvFile() error {
+	return godotenv.Load()
+}
+
+func loadConfigValue(valueDescriptor *Descriptor) error {
+	switch valueDescriptor.TypeInfo.Type {
+	case StringType:
+		return loadStringValue(valueDescriptor)
+	case BoolType:
+		return loadBoolValue(valueDescriptor)
+	case IntType:
+		return loadIntValue(valueDescriptor)
+	default:
+		return fmt.Errorf("unknown type %s", valueDescriptor.TypeInfo.Type)
+	}
+}
+
+func loadStringValue(valueDescriptor *Descriptor) error {
+	value, ok := os.LookupEnv(valueDescriptor.EnvionmentVariable)
+
+	if !ok {
+		if valueDescriptor.Default != nil {
+			valueDescriptor.Value = valueDescriptor.Default.(string)
+			applicationConfig.StringValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+			return nil
+		} else {
+			return fmt.Errorf("missing environment variable %s", valueDescriptor.EnvionmentVariable)
+		}
+	}
+
+	if valueDescriptor.NotEmpty && value == "" {
+		return fmt.Errorf("environment variable %s must not be empty", valueDescriptor.EnvionmentVariable)
+	}
+
+	valueDescriptor.Provided = true
+	valueDescriptor.Value = value
+	applicationConfig.StringValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+
+	return nil
+}
+
+func loadBoolValue(valueDescriptor *Descriptor) error {
+	value, ok := os.LookupEnv(valueDescriptor.EnvionmentVariable)
+
+	if !ok {
+		if valueDescriptor.Default != nil {
+			valueDescriptor.Value = valueDescriptor.Default.(bool)
+			applicationConfig.BoolValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+			return nil
+		} else {
+			return fmt.Errorf("missing environment variable %s", valueDescriptor.EnvionmentVariable)
+		}
+	}
+
+	if strings.ToLower(value) != "true" && strings.ToLower(value) != "false" {
+		return fmt.Errorf("environment variable %s must be either 'true' or 'false'", valueDescriptor.EnvionmentVariable)
+	}
+
+	valueDescriptor.Provided = true
+	valueDescriptor.Value = strings.ToLower(value) == "true"
+	applicationConfig.BoolValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+
+	return nil
+}
+
+func loadIntValue(valueDescriptor *Descriptor) error {
+	value, ok := os.LookupEnv(valueDescriptor.EnvionmentVariable)
+
+	if !ok {
+		if valueDescriptor.Default != nil {
+			valueDescriptor.Value = valueDescriptor.Default.(int)
+			applicationConfig.IntValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+			return nil
+		} else {
+			return fmt.Errorf("missing environment variable %s", valueDescriptor.EnvionmentVariable)
+		}
+	}
+
+	number, err := strconv.Atoi(value)
+	if err != nil {
+		return fmt.Errorf("environment variable %s must be a valid integer", valueDescriptor.EnvionmentVariable)
+	}
+
+	valueDescriptor.Provided = true
+	valueDescriptor.Value = number
+	applicationConfig.IntValues[valueDescriptor.EnvionmentVariable] = valueDescriptor
+
+	return nil
+}
+
+func Print() {
+	t := table.NewWriter()
+	t.SetTitle("Application Configuration")
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"ENV VAR", "Type", "Default Value", "Value Provided", "Current Value"})
+
+	for _, value := range applicationConfig.StringValues {
+		t.AppendRow(table.Row{value.EnvionmentVariable, "string", GetSanatizedDefaultValue(value), value.Provided, GetSensitiveProtectedValue(value)})
+	}
+	for _, value := range applicationConfig.BoolValues {
+		t.AppendRow(table.Row{value.EnvionmentVariable, "bool", GetSanatizedDefaultValue(value), value.Provided, GetSensitiveProtectedValue(value)})
+	}
+	for _, value := range applicationConfig.IntValues {
+		t.AppendRow(table.Row{value.EnvionmentVariable, "int", GetSanatizedDefaultValue(value), value.Provided, GetSensitiveProtectedValue(value)})
+	}
+	t.SortBy([]table.SortBy{
+		{Name: "ENV VAR", Mode: table.Asc},
+	})
+	t.SetStyle(table.StyleColoredYellowWhiteOnBlack)
+	t.Render()
+}
+
+func GetSensitiveProtectedValue(valueDescriptor *Descriptor) string {
+	if valueDescriptor.Sensitive {
+		switch valueDescriptor.TypeInfo.Type {
+		case StringType:
+			return strings.Repeat("*", len(valueDescriptor.Value.(string)))
+		case BoolType:
+			return "****"
+		case IntType:
+			return strings.Repeat("*", len(strconv.Itoa(valueDescriptor.Value.(int))))
+		default:
+			return "****"
+		}
+	} else {
+		return fmt.Sprintf("%v", valueDescriptor.Value)
+	}
+}
+
+func GetSanatizedDefaultValue(valueDescriptor *Descriptor) string {
+	if valueDescriptor.Default == nil {
+		return "-"
+	} else {
+		return fmt.Sprintf("%v", valueDescriptor.Default)
+	}
+}
